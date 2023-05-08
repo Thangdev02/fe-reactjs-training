fd8aa26a0b1b41bdc59cf4d6319b0ffa675e87df
Macbook Air <macbookair@Macbooks-QuocThang.local>
2023-05-08 20:28:20 +0700

    Proficient Navigation

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/pages/MyHome';
 import MemberList from './components/pages/MemberList';
 import MemberDetails from './components/pages/MemberDetails';
-
+import { routes } from './routes/routes';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element ={<HomePage/>} />
-        <Route path="/memberlist" element ={<MemberList/>} />
-        <Route path="/memberdetails" element ={<MemberDetails/>} />
-
+        <Route path={routes.home} element ={<HomePage/>} />
+        <Route path={routes.memberlist} element ={<MemberList/>} />
+        <Route path={routes.memberdetails} element ={<MemberDetails/>} />
         </Routes>
     </div>
   );
---
+++ src/routes/routes.js
@@ -0,0 +1,5 @@
+export const routes = {
+    home:"/",
+    memberlist: "/memberlist",
+    memberdetails: "/memberdetails"
+}
