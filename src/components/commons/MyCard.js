f6f56a3be6d08acb5c7bdce902f9167aa11f67db
Macbook Air <macbookair@Macbooks-QuocThang.local>
2023-05-08 20:12:47 +0700

    Using extension

--- src/components/commons/MyCard.js
+++ src/components/commons/MyCard.js
@@ -16,6 +16,5 @@ const MyCard = (props) => (
       </div>
     </Card>
   </Col>
-
 );
 export default MyCard;
--- src/components/pages/MemberList.js
+++ src/components/pages/MemberList.js
@@ -3,7 +3,6 @@ import MyCard from "../commons/MyCard";
 import { members } from "../../utils/Data";
 import { useNavigate } from "react-router-dom"
 import "../../styles/MemberList.css"
-import MyButton from "../commons/MyButton";
 
 
 function MemberList() {
@@ -16,7 +15,7 @@ function MemberList() {
             <div className="members">
                 {members.map((member) => (
 
-                        <MyCard className="member" key={member.index} title={member.name} text={member.position} onClick={() => handleClick(member)} bttext="View Details" />
+                    <MyCard className="member" key={member.index} title={member.name} text={member.position} onClick={() => handleClick(member)} bttext="View Details" />
 
                 ))}
             </div>
