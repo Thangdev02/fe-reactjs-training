1b1b24ad45bae3062653b773b3302e515b4454bd
Macbook Air <macbookair@Macbooks-QuocThang.local>
2023-05-10 17:13:52 +0700

    edit hompage in package.json

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 export const routes = {
-  home: "/fe-reactjs-training/",
-  login: "/fe-reactjs-training/login",
-  register: "/fe-reactjs-training/register",
-
+  home: "/",
+  login: "/login",
+  register: "/register",
+  
 };
